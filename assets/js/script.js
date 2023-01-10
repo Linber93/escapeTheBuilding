01a14ed15de96ee57af441469eb49c7c6309d19a
Linber93 <linus@nybyn.se>
2023-01-10 15:38:32 +0000

    fixed player position and player movement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,41 @@
 //wait for dom to finish loading before runnning the game.
 //add game area and buttons with event listeners
 
+var game = {
+    /* level layout */
+    layout: [
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 0, 0, 0]
+    ],
+    player_pos_x: 0,
+    player_pos_y: 0
+}
+
 document.addEventListener("DOMContentLoaded", function() {
         /* creates game grid*/
 
         let building = document.querySelector('.game-building');
         let width = 28;
-        /* level layout */
-        let layout = [
-            [0, 1, 0, 0, 0, 0],
-            [0, 1, 0, 1, 1, 0],
-            [0, 0, 0, 0, 1, 0],
-            [1, 1, 1, 1, 1, 0],
-            [0, 0, 0, 0, 1, 0],
-            [0, 1, 1, 0, 0, 0]
-        ];
 
         let gameSquares = [];
 
         function createBoard() {
-            for (let i = 0; i < layout.length; i++) {
+            for (let i = 0; i < game.layout.length; i++) {
                 gameSquares[i] = [];
-                for(let j = 0; j < layout[i].length; j++) {
+                for(let j = 0; j < game.layout[i].length; j++) {
                     let location = document.createElement('div');
                     building.appendChild(location);
                     gameSquares[i][j] = location;
             
                     //add layout to the board
-                    if (layout[i][j] === 1) {
+                    if (game.layout[i][j] === 1) {
                         gameSquares[i][j].classList.add('wall');
                     }
-                    if (layout[i][j] === 0) {
+                    if (game.layout[i][j] === 0) {
                         gameSquares[i][j].classList.add('corridor');
                     }
                 }
@@ -47,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "start-game"){
                 startGametimer();
             } else if (this.getAttribute("data-type") === "leaderboard"){
-                goToLeaderboard();
+                showLeaderboard();
             } else {
                 alert("Something went wrong!")
             }
@@ -78,20 +83,34 @@ function runGame() {
     }
 }
 
-function playerPositionUp(layout) {
-  
+
+function playerMove(x, y) {
+    var new_pos_x = Math.max(0, Math.min(5, game.player_pos_x+x));
+    var new_pos_y = Math.max(0, Math.min(5, game.player_pos_y+y));
+    if (game.layout[new_pos_y][new_pos_x] == 0) {
+        game.player_pos_x = new_pos_x;
+        game.player_pos_y = new_pos_y;
+        console.log('New pos: '+game.player_pos_x+', '+game.player_pos_y);
+    }
+    else {
+        console.log('Invalid position');
+    }
+}
+
+function playerPositionUp() {
+    playerMove(0,-1);
 } 
 
-function playerPositionLeft(layout) {
-  
+function playerPositionLeft() {
+    playerMove(-1,0);  
 }
 
-function playerPositionRight(layout) {
-  
+function playerPositionRight() {
+    playerMove(1,0);
 }
 
-function playerPositionDown(layout) {
-  
+function playerPositionDown() {
+    playerMove(0,1);
 }
 
 
@@ -117,6 +136,27 @@ function startGametimer() {
             console.log('game is running')
         }
     }, 1000);
+}
+
+
+
+function showLeaderboard() {
+    let overlay = document.querySelector('#leaderboard-overlay');
+    let leaderboardArea = document.createElement('div');
+    let exitLeaderboard = document.createElement('button');
+
+    overlay.textContent = '';
+    exitLeaderboard.classList.add('btn-exit-leaderboard');
+    leaderboardArea.classList.add('leaderboard-area');
+    leaderboardArea.appendChild(exitLeaderboard);
+    overlay.appendChild(leaderboardArea);
+    exitLeaderboard.addEventListener("click", hideLeaderboard);
+
+    document.getElementById('leaderboard-overlay').style.display = 'block';
+}
+
+function hideLeaderboard() {
+    document.getElementById('leaderboard-overlay').style.display = 'none';
 }
 
 
